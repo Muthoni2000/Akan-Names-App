6d9ff88d34dcce3a765e1e3825926cfecf6854fc
Muthoni2000 <bmuthoni39@yahoo.com>
2020-02-16 17:19:20 +0300

    Check if values entered are correct

--- js/script.js
+++ js/script.js
@@ -6,5 +6,17 @@ var months=["January","February","March","April","May","June","July","August","S
 
 //Day of week function
 function compute(form){
-
+    var val1=parseInt(form.day.value,10);
+    if((val1<=0)||(val1>31)){
+        alert("Invalid day! Kindly input the correct day")
+    }
+    var val2=parseInt(form.month.value,10);
+    if ((val2<=0)||(val2>12)){
+        alert("Invalid month! Kindly input the correct month")
+    }
+    var val2x=parseInt(form.month.value,10);
+    var val3=parseInt(form.year.value,10);
+    if (val3<1900){
+        alert("Are you that old?!")
+    }
 }
