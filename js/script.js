198a66a1dfce27c8d150fb49f51c5aa326d62b50
Muthoni2000 <bmuthoni39@yahoo.com>
2020-02-16 17:44:26 +0300

    Add gh pages

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var days=["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
 
 //Initialize Months Array
-var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+var months=["undefined","January","February","March","April","May","June","July","August","September","October","November","December"];
 
 //Day of week function
 function compute(form){
