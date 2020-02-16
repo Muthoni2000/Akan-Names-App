b6caecab1f83bdebcc255097075789886cd6c29c
Muthoni2000 <bmuthoni39@yahoo.com>
2020-02-16 17:18:09 +0300

    Create a function for computation

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ var days=["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"
 
 //Initialize Months Array
 var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+//Day of week function
+function compute(form){
+
+}
