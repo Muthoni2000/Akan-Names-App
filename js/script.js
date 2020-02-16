a318359bcf69f6a96e7cf27319c0bc0fe7f89b3c
Muthoni2000 <bmuthoni39@yahoo.com>
2020-02-16 17:12:55 +0300

    Initialize Months array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 //Initialize Days of Week Array
 var days=["Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
+
+//Initialize Months Array
+var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
